add88c28ba6f1998c2718ef9a30c543b28249327
Godzzo <egodzzo@gmail.com>
2020-03-13 09:25:19 +0100

    AgentDebugger added for Bots

---
+++ Assets/Scripts/NavMeshAgentDebugger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentDebugger : MonoBehaviour
+{
+    LineRenderer _renderer;
+    [SerializeField] NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_agent.hasPath) {
+            _renderer.positionCount = _agent.path.corners.Length;
+            _renderer.SetPositions(_agent.path.corners);
+            _renderer.enabled = true;
+        } else {
+            _renderer.enabled = false;
+        }
+    }
+}
