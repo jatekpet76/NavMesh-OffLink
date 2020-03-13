d36cafb2e3204e172c10556fe4415d795e5baf7f
Godzzo <egodzzo@gmail.com>
2020-03-13 08:56:59 +0100

    Microby working with mouse but Adam and Eve with waitpoints not

--- Assets/Scripts/GoToController.cs
+++ Assets/Scripts/GoToController.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GoToController : MonoBehaviour
 {
+    [SerializeField] List<Transform> _waitpoints;
+	int _next = 0;
+	NavMeshAgent _agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		_agent = GetComponent<NavMeshAgent>();
+
+		StartCoroutine(WalkingBetweenWaitpoints());
     }
+
+	IEnumerator WalkingBetweenWaitpoints() {
+		while (true) {
+			yield return new WaitForSeconds(3);
+
+			MoveTo();
+		}
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void MoveTo()
+    {
+		if (_agent.destination == null || Vector3.Distance(transform.position, _waitpoints[_next].position) < 0.3) {
+			_agent.destination = _waitpoints[_next].position;
+			_next++;
+
+			if (_next > _waitpoints.Count-1) {
+				_next = 0;
+
+				Debug.Log("Go back to First one");
+			}
+		}
     }
 }
---
+++ Assets/Scripts/MouseInputController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseInputController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100)) {
+                _agent.destination = hit.transform.position;
+            }
+        }
+    }
+}
